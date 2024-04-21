d73323591143d2e281a46c07e336cdc12e485939
luisa2927 <adrianosoriogomez1976@gmail.com>
2024-04-21 18:04:12 -0500

    actualizaciones

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
- import conexionMongo from "./src/config/baseDatos.js";
+import conexionMongo from "./src/config/baseDatos.js";
 
 
 //2. CONFIGURAR NUESTRO SERVIDOR
@@ -11,23 +11,25 @@ const puerto = 9000
 
 //2.1 CONFIGURAR LAS VARIABLE DE ENTORNOS
 dotenv.config();
+
+//2.2 CONFIGURAR CONEXION BASE DE DATOS
 conexionMongo();
 
 
 // ESTABLECER CONECCION CON NUESTRO INDEX HTML
-const rutaPublica = path.join(process.cwd(), 
-"public");
+const rutaPublica = path.join(process.cwd(),
+    "public");
 app.use(express.static(rutaPublica));
 
 app.use(express.json());
 
 //Especificamos que vamos a caceder a index.html
 app.get("/", (req, res) => {
-    res.sendFile(path.join(rutaPublica,"index.html"));
+    res.sendFile(path.join(rutaPublica, "index.html"));
 });
 //4. INICIALIZAR NUESTRO SERVIDOR
 
 
 app.listen(puerto, () => {
-    console.log(`El servidor está escuchando en http://localhost:${puerto}`); 
+    console.log(`El servidor está escuchando en http://localhost:${puerto}`);
 });
--- src/config/baseDatos.js
+++ src/config/baseDatos.js
@@ -1,13 +1,20 @@
 // importamos las dependencias que necesitamos
 import mongoose from "mongoose";
+
+// crear una conexion que nos conecta con la base de datos
 const conexionMongo = async () => {
-    await mongoose.connect(BD_URL,{})
-    //control de errores
+
+    await mongoose.connect(process.env.BD_URL,{})
+
+    //control de errores con try - catch
     try{
         console.log("conexion exitosa");
     }catch(error){
         console.log("error de conexion",error.message);
     }
 }
+
+
+// tenemos que exportar nuestra funcion para llamarla desde nuestro servidor
 
 export default conexionMongo;
