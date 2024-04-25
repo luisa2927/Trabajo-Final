198ae5075c335bd6ea2c5fc73a17d0216f41d0b4
luisa2927 <adrianosoriogomez1976@gmail.com>
2024-04-24 21:19:10 -0500

    actualizacion

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -15,20 +15,59 @@ PETICIONES
 
 //LÓGICA PARA MOSTRAR USUARIOS
 export const getUsuario = async(req, res) => {
-    return res.send('Funciona la petición GET');
+
+    //Manejo de errores con try y catch
+    try{
+        let usuarios = await usuarioModel.find();
+        return res.send(usuarios);
+    }catch(error){
+        return res.json({error: "error al mostrar los datos" + error });
+    }
+
 }
 
 //LÓGICA PARA CREAR USUARIOS
 export const postUsuario = async(req, res) => {
-    return res.send('Funciona la petición POST');
+    //manejo de errores con try y catch
+
+    try{
+        let datosUsuario = req.body;
+        let nuevoUsuario = await usuarioModel.create(datosUsuario);
+        return res.json(nuevoUsuario);
+
+    }catch(error){
+        //Para que me muestre un mensaje que me indique cuál es el error
+        return res.json({error: "error al crear el usuario, holi", message:error.message});
+    }
+
 }
 
-//LÓGICA PARA MODIFICAR USUARIOS
+///LÓGICA PARA MODIFICAR USUARIOS POR ID
 export const putUsuario = async(req, res) => {
-    return res.send('Funciona la petición PUT');
-}
+    try{
+        let datosModificar = req.body;
+        let idModificar = req.params._id;
 
-//LÓGICA PARA ELIMINAR USUARIOS
+        await usuarioModel.findByIdAndUpdate(idModificar,datosModificar);
+        return res.json({message:"Usuario actualizado correctamente"});
+        
+    }catch(error){
+        return res.json({error: "error al modificar usuario", message:error.message});
+    }
+}
+//LÓGICA PARA ELIMINAR USUARIOS POR ID
 export const deleteUsuario = async(req, res) => {
-    return res.send('Funciona la petición DELETE');
+    try{
+        let idEliminar = req.params._id;
+        let usuarioEliminado = await usuarioModel.findByIdAndDelete(idEliminar);
+
+        if(usuarioEliminado){
+            return res.json({message: "Usuario eliminado correctamente"});
+        } else{
+            return res.json({message: "Ups! no se pudo eliminar ese usuario"});
+        }
+
+    }catch(error){
+        return res.json({error: "error al eliminar usuario", message:error.message});
+    }
 }
