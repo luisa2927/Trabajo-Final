62cf7489d4a632319629c3188902b61053a98a85
luisa2927 <adrianosoriogomez1976@gmail.com>
2024-04-29 20:06:38 -0500

    guardar canbios

---
+++ public/scrits/registro.js
@@ -0,0 +1,42 @@
+1. // verificar coneccion con html
+console.log("hola soy registro");
+
+2. // crear mi funcion para registro
+
+const registrarUsuario =async ()=> {}
+
+    // obtener los datos de mi formulario
+    const nombreCompleto = document.getElementById
+    ("nonbreCompleto").value;
+    const correo = document.getElementById("correo").
+    value;
+    const contrasena = document.getElementById("contrasena").value;
+
+    console.log(nombreCompleto,correo,contrasena);
+
+    //2.2crear un objeto con los datos del usuario
+    const datosUsuario = {
+        nombreCompleto,
+        correo,
+        contrasena  
+    }
+
+    console.log(datosUsuario); 
+
+  //2.3 Hacer la peticion POST para crear usuario en nuestra base de datos
+
+  try{
+   
+    const respuesta = await fetch("http://localhost:3000/api/usuarios",
+    {
+        method:"POST",
+        headers:{"contemt-type":"application/json"},
+        body:JSON.stringify(datosUsuario)
+    }
+);
+  }catch(error){
+    console.error("Error al registrar usuario"), error
+
+
+
+  }
